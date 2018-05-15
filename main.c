b523376dc7d63905e6bb00a623fc4a98ecf5afba
spirits3 <flo_diablo@hotmail.com>
2018-05-15 11:46:11 +0200

    saisie faites

--- main.c
+++ main.c
@@ -4,12 +4,20 @@
 int main(void) {
 
 	int nombreBille;
-
-	printf("%s", "Entrez le nombre de billes [1000 - 30000] :");
+	int nombreCompteur;
 
 	do {
-		scanf("%d", &nombreBille);		
-	} while(nombreBille <= 1000 || nombreBille >=3000)
+		printf("%s", "Entrez le nombre de billes [1000 - 30000] :");
+		scanf("%4d", &nombreBille);
+	    while (getchar()!='\n');
 
-	return 0;
+	} while(nombreBille < 1000 || nombreBille > 3000);
+
+	do {
+		printf("%s", "Entrez le nombre de rangees de compteurs [10 - 20] :");
+		scanf("%2d", &nombreCompteur);
+		while(getchar() != '\n');
+	} while(nombreCompteur < 10 || nombreCompteur > 20);
+
+	return EXIT_SUCCESS;
 }
